0dbcad1a5c8ded85a43577a4acbed290d112d218
kavin0506 <kavinmoorthy@gmail.com>
2024-03-28 16:28:49 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BmiCalculator from './BmiCalculator';
+
+function App() {
+  return (
+    <div className="App">
+     <BmiCalculator/>
+    </div>
+  );
+}
+
+export default App;
